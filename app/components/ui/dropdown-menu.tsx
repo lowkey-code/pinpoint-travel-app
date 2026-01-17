0999ea2e1157d71c17168e375b7c0ac89131bd9e
Matheus Solha <mcsolha@gmail.com>
2026-01-17 19:14:51 -0300

    Restore Ark UI menu portal usage

--- app/components/ui/dropdown-menu.tsx
+++ app/components/ui/dropdown-menu.tsx
@@ -13,9 +13,9 @@ function DropdownMenu({
 }
 
 function DropdownMenuPortal({
-  children,
-}: React.PropsWithChildren) {
-  return <>{children}</>
+  ...props
+}: React.ComponentProps<typeof Menu.Portal>) {
+  return <Menu.Portal data-slot="dropdown-menu-portal" {...props} />
 }
 
 function DropdownMenuTrigger({
@@ -32,16 +32,18 @@ function DropdownMenuContent({
   sideOffset?: number
 }) {
   return (
-    <Menu.Positioner style={{ marginTop: sideOffset }}>
-      <Menu.Content
-        data-slot="dropdown-menu-content"
-        className={cn(
-          "bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 max-h-(--available-height) min-w-[8rem] origin-(--transform-origin) overflow-x-hidden overflow-y-auto rounded-md border p-1 shadow-md",
-          className
-        )}
-        {...props}
-      />
-    </Menu.Positioner>
+    <Menu.Portal>
+      <Menu.Positioner style={{ marginTop: sideOffset }}>
+        <Menu.Content
+          data-slot="dropdown-menu-content"
+          className={cn(
+            "bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 max-h-(--available-height) min-w-[8rem] origin-(--transform-origin) overflow-x-hidden overflow-y-auto rounded-md border p-1 shadow-md",
+            className
+          )}
+          {...props}
+        />
+      </Menu.Positioner>
+    </Menu.Portal>
   )
 }
 
